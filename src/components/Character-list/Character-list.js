a4360b0602271d794bfcca4e7fad489c9b88e881
Dasha <asterdeveris@gmail.com>
2023-03-16 12:10:06 +0200

    add fetching info for list of characters, add loading display

--- src/components/Character-list/Character-list.js
+++ src/components/Character-list/Character-list.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CharacterListItem from "../Character-list-item/Character-list-item";
+import { getCharacters } from "rickmortyapi";
+import spinner from "./DoubleRing.svg";
 import "./Character-list.scss";
 
 const CharacterList = () => {
-  return (
-    <div className="list-container">
-      <CharacterListItem />
-      <CharacterListItem />
-      <CharacterListItem />
-      <CharacterListItem />
-      <CharacterListItem />
-      <CharacterListItem />
-      <CharacterListItem />
-      <CharacterListItem />
-    </div>
-  );
+  const [listOfCharacters, setListOfCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const func = async () => {
+      const res = await getCharacters();
+      setListOfCharacters(res.data.results);
+      setIsLoading(false);
+    };
+    func();
+  }, []);
+
+  const populatedList = listOfCharacters.map(({ name, species, image, id }) => {
+    return (
+      <CharacterListItem name={name} image={image} species={species} key={id} />
+    );
+  });
+
+  if (isLoading) {
+    return <img src={spinner} />;
+  }
+  return <div className="list-container">{populatedList}</div>;
 };
 
 export default CharacterList;
---
+++ src/components/Main-page/main-page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Header/Header";
+import CharacterList from "../Character-list/Character-list";
+
+const MainPage = () => {
+  return (
+    <>
+      <Header />
+      <CharacterList />
+    </>
+  );
+};
+
+export default MainPage;
