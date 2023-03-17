ed182665cc63f3070e4c0cc9c59b0b9d1ae01c32
Dasha <asterdeveris@gmail.com>
2023-03-17 15:09:24 +0200

    add component for login page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import MainPage from "../Main-page/main-page";
 import CharacterDetails from "../Character-details/Character-details";
+import LoginPage from "../Login-page/Login-page";
 import "./App.scss";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/character-details/:id" element={<CharacterDetails />} />
+        <Route path="/login-page" element={<LoginPage />} />
       </Routes>
     </div>
   );
---
+++ src/components/Login-page/Login-page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Login-page.scss";
+
+const LoginPage = () => {
+  return (
+    <div className="login-page">
+      <h1>Log in</h1>
+      <button>Continue with Google</button>
+      <button>Continue with Facebook</button>
+    </div>
+  );
+};
+
+export default LoginPage;
