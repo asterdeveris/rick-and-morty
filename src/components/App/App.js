9cbc96407eeeee48ba04f11c240401ffd4eda56b
Dasha <asterdeveris@gmail.com>
2023-03-22 19:23:33 +0200

    delete backend

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import MainPage from "../Main-page/main-page";
 import CharacterDetails from "../Character-details/Character-details";
-import LoginPage from "../Login-page/Login-page";
-import Navbar from "../Navbar/Navbar";
 import "./App.scss";
 
 function App() {
@@ -40,20 +38,7 @@ function App() {
     <div className="container">
       <Routes>
         <Route path="/" element={<MainPage user={user} />} />
-        <Route
-          path="/character-details/:id"
-          element={
-            user ? (
-              <CharacterDetails user={user} />
-            ) : (
-              <Navigate to="/login-page" />
-            )
-          }
-        />
-        <Route
-          path="/login-page"
-          element={user ? <Navigate to="/" /> : <LoginPage />}
-        />
+        <Route path="/character-details/:id" element={<CharacterDetails />} />
       </Routes>
     </div>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 import arrowIcon from "./arrow_back.svg";
 import "./Navbar.scss";
 
-const Navbar = ({ user, isMainPage }) => {
+const Navbar = ({ isMainPage }) => {
   const logOut = () => {
     window.open("http://localhost:5000/auth/logout", "_self");
   };
-  return user ? (
+  return (
     <nav className="navbar">
       <Link to="/">
         <div
@@ -18,39 +18,6 @@ const Navbar = ({ user, isMainPage }) => {
           Go back
         </div>
       </Link>
-      <ul>
-        <li>
-          <img
-            src={user.photos[0].value}
-            alt="user"
-            referrerPolicy="no-referrer"
-          />
-        </li>
-        <li>
-          <button className="navbar-btn" onClick={logOut}>
-            Logout
-          </button>
-        </li>
-      </ul>
-    </nav>
-  ) : (
-    <nav className="navbar">
-      <Link to="/">
-        <div
-          className="back-to-main-link"
-          style={isMainPage ? { display: "none" } : { display: "block" }}
-        >
-          <img src={arrowIcon} />
-          Go back
-        </div>
-      </Link>
-      <ul>
-        <li>
-          <Link to="/login-page">
-            <button className="navbar-btn">Login</button>
-          </Link>
-        </li>
-      </ul>
     </nav>
   );
 };
